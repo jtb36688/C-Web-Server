edc873f953334f0fe6e55cb1ed240132219bd4c6
Jacob Bryan <jtb36688@gmail.com>
2019-07-08 18:38:25 -0400

    attempting to build server's http response from GP provided code

--- src/server.c
+++ src/server.c
@@ -55,9 +55,13 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 
     // Build HTTP response and store it in response
 
-    ///////////////////
-    // IMPLEMENT ME! //
-    ///////////////////
+sprintf(response, "HTTP/1.1 200 OK\n"
+                    "Content-Type: text/html\n"
+                    "Content-Length: %d\n"
+                    "Connection: close\n"
+                    "\n"
+                    "%s",
+                    content_length, body);
 
     // Send it all!
     int rv = send(fd, response, response_length, 0);
